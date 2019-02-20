f85eca7b241f2fcaed0c62c682849f6236cdfad3
Phú Phỡn <phanthanhphu2003_8@live.com>
2019-02-20 23:11:15 +0700

    update layout & add vue

---
+++ Up/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Up.Services;
+
+namespace Up.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IKhoaHocService _khoaHocService;
+
+        public CategoryController(IKhoaHocService khoaHocService)
+        {
+            _khoaHocService = khoaHocService;
+        }
+
+        public IActionResult KhoaHocIndex()
+        {
+            return View();
+        }
+    }
+}
