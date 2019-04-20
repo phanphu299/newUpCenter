3abbec914afe4cb31070858ba3dcdd4ccf1051dc
Phú Phỡn <phanthanhphu2003_8@live.com>
2019-04-20 16:11:44 +0700

    add some component for diemdanh

--- Up/wwwroot/js/diemdanhindex.js
+++ Up/wwwroot/js/diemdanhindex.js
@@ -7,12 +7,33 @@
         timeout: 3000,
         snackbar: false,
         deleteDialog: false,
-        dialog: false
+        dialog: false,
+        selectedLopHoc: '',
+        ngayDiemDanh: new Date().toISOString().substr(0, 10),
+        itemLopHoc: [],
+        isShowDatePicker: false,
+        hocVienList: []
     },
     async beforeCreate() {
-        
+        let that = this;
+        await axios.get('/LopHoc/GetAvailableLopHocAsync')
+            .then(function (response) {
+                that.itemLopHoc = response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     },
     methods: {
-       
+        async GetHocVienByLopHoc() {
+            let that = this;
+            await axios.get('/DiemDanh/GetHocVienByLopHocAsync?LopHocId=' + this.selectedLopHoc)
+                .then(function (response) {
+                    that.hocVienList = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
     }
 });
